8f635da90081bda613a5c46ec65174ec102c2ee9
Jeroen Berkvens <J.Berkvens@Moridrin.com>
2014-11-24 13:08:03 +0100

    Opdracht 1.2 (Rollback) uitgewerkt.

--- bank/src/test/java/util/Opdracht1.java
+++ bank/src/test/java/util/Opdracht1.java
@@ -5,7 +5,12 @@
  */
 package util;
 
+import bank.dao.AccountDAOJPAImpl;
 import bank.domain.Account;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -39,20 +44,23 @@ public class Opdracht1 {
 
     @Before
     public void setUp() {
-
         em = emf.createEntityManager();
     }
 
     @After
     public void tearDown() {
-        em.close();
+        try {
+            DatabaseCleaner.clean(em);
+        } catch (SQLException ex) {
+            Logger.getLogger(Opdracht1.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
     @Test
-    public void hello() {
+    public void persistCommitTest() {
         Account account = new Account(112L);
         em.getTransaction().begin();
         em.persist(account);
@@ -68,5 +76,21 @@ public class Opdracht1 {
          * komen dus ook alle database errors naar boven (zoals Duplicate Key).
          */
         assertTrue(account.getId() > 0L);
+    }
+
+    @Test
+    public void rollbackTest() {
+        Account account = new Account(111L);
+        em.getTransaction().begin();
+        em.persist(account);
+        assertNull(account.getId());
+        em.getTransaction().rollback();
+        AccountDAOJPAImpl accountDAOJPAImpl = new AccountDAOJPAImpl(em);
+        List<Account> findAll = accountDAOJPAImpl.findAll();
+        assertEquals(findAll.size(), 0);
+        /**
+         * Met de functie findAll() laad hij alles wat hij kan vinden. Als daar
+         * dus geen records gevonden worden (size=0) is de database dus leeg.
+         */
     }
 }
